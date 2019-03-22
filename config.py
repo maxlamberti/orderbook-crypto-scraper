ed8ad853a77ed9394d8d71e2cea1f1e9452246fd
maxlamberti <maximilien.lamberti@gmail.com>
2019-03-22 12:46:28 -0700

    add logging config

---
+++ config.py
@@ -0,0 +1,24 @@
+
+
+LOGGING = {
+	'disable_existing_loggers': False,
+	'version': 1,
+	'formatters': {
+		'simple': {
+			'format': '%(asctime)s - %(levelname)s - %(message)s'
+		},
+	},
+	'handlers': {
+		'console': {
+			'level': 'DEBUG',
+			'formatter': 'simple',
+			'class': 'logging.StreamHandler',
+		}
+	},
+	'loggers': {
+		'scraper': {
+			'handlers': ['console'],
+			'level': 'INFO',
+		}
+	},
+}
